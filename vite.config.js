68773323b319eec5ccee4f455ad57ae7c6525f2a
Taarik Ashenafi <147209483+taarikashenafi@users.noreply.github.com>
2026-08-07 21:41:19 -0500

    Add serial test execution

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
+    fileParallelism: false,
     coverage: {
       provider: 'v8',
       include: ['server/**/*.js'],
